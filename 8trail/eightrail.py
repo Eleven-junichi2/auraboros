1333107fdee3179dcfae0b28345e15ceb28b7bb7
Eleven-junichi2 <junichinomori@yahoo.co.jp>
2022-10-29 20:23:44 +0900

    pixel resizing

--- 8trail/eightrail.py
+++ 8trail/eightrail.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+import sys
 import pygame
 
 pygame.init()
@@ -7,43 +8,42 @@ clock = pygame.time.Clock()
 
 
 class AssetFilePath:
-    def __init__(self, root_path):
-        self.root = Path(root_path)
-        self.img_dirname = Path("imgs")
-        self.sound_dirname = Path("sounds")
+    root = Path(sys.argv[0]).parent / "assets"
+    img_dirname = "imgs"
+    sound_dirname = "sounds"
 
-    def img(self, filename):
-        return self.root / self.img_dirname / filename
+    @classmethod
+    def img(cls, filename):
+        return cls.root / cls.img_dirname / filename
 
 
 class Player(pygame.sprite.Sprite):
 
-    # Constructor. Pass in the color of the block,
-    # and its x and y position
     def __init__(self, *args, **kwargs):
-        # Call the parent class (Sprite) constructor
         super(Player, self).__init__(args, kwargs)
-        # Create an image of the block, and fill it with a color.
-        # This could also be an image loaded from the disk.
-        self.image = pygame.Surface([32, 32])
-        self.image.fill(pygame.Color("#6495ed"))
+        self.image = pygame.image.load(AssetFilePath.img("fighter_a.png"))
         self.rect = self.image.get_rect()
 
     def draw(self, screen: pygame.surface.Surface):
         screen.blit(self.image, self.rect)
 
 
-def init(window_size=(960, 640), caption=""):
+def init(window_size=(960, 640), caption="", pixel_scale=2):
     global screen
     global w_size
-    w_size = window_size
-    screen = pygame.display.set_mode((w_size[0], w_size[1]))
+    global w_size_unscaled
+    pixel_scale = pixel_scale
+    w_size_unscaled = window_size
+    w_size = tuple([length // pixel_scale for length in window_size])
+    pygame.display.set_mode(w_size_unscaled)
+    screen = pygame.Surface(w_size)
+
     pygame.display.set_caption(caption)
 
 
 def run(fps=60):
     player = Player()
-    player.rect.x = w_size[0] / 2
+    player.rect.x = w_size[0] / 2 - player.rect.width
     player.rect.y = w_size[1] - player.rect.height
     running = True
     while running:
@@ -51,12 +51,20 @@ def run(fps=60):
             if event.type == pygame.QUIT:
                 running = False
             if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    player.rect.y -= 1
+                if event.key == pygame.K_DOWN:
+                    player.rect.y += 1
                 if event.key == pygame.K_LEFT:
                     player.rect.x -= 1
                 if event.key == pygame.K_RIGHT:
                     player.rect.x += 1
+
         screen.fill((0, 0, 0))
         player.draw(screen)
-        pygame.display.flip()
+        # ↓ resize pixel size
+        pygame.transform.scale(screen, w_size_unscaled,
+                               pygame.display.get_surface())
+        pygame.display.update()
         clock.tick(fps)
     pygame.quit()
