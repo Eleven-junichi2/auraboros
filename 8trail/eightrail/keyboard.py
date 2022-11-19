cdbdeb990be9371ed2d5b7ad9e6fda1af4eb2857
Eleven-junichi2 <junichinomori@yahoo.co.jp>
2022-11-19 17:15:38 +0900

    implement keyboard module on title menu

--- 8trail/eightrail/keyboard.py
+++ 8trail/eightrail/keyboard.py
@@ -1,33 +1,58 @@
-from collections import UserDict
-from typing import Callable
+# from collections import UserDict
+from typing import Callable, TypedDict
 
-# import pygame
+import pygame
 
 
 class Keyboard:
     def __init__(self):
-        self.keyaction_dict = {}
+        self.keyaction_dict: dict[int, KeyActionItem] = {}
+
+    def __getitem__(self, key):
+        return self.keyaction_dict[key]
+
+    def event(self, event):
+        if event.type == pygame.KEYDOWN:
+            if self.keyaction_dict.get(event.key):
+                self.keyaction_dict[event.key]["is_pressed"] = True
+        if event.type == pygame.KEYUP:
+            if self.keyaction_dict.get(event.key):
+                self.keyaction_dict[event.key]["is_pressed"] = False
+
+    def action_by_keyinput(self, pygame_key_const):
+        # run keyaction whether the key is pressed
+        if self.keyaction_dict[pygame_key_const]['is_pressed']:
+            if self.keyaction_dict[pygame_key_const]['_interval_counter'] == 0:
+                self.keyaction_dict[pygame_key_const]['_interval_counter'] += 1
+                return self.keyaction_dict[pygame_key_const]["keydown"]()
+            elif self.keyaction_dict[
+                    pygame_key_const][
+                        '_interval_counter'] == self.keyaction_dict[
+                        pygame_key_const]['interval']:
+                self.keyaction_dict[pygame_key_const]['_interval_counter'] = 0
+                return self.keyaction_dict[pygame_key_const]["keydown"]()
+            else:
+                self.keyaction_dict[pygame_key_const]['_interval_counter'] += 1
+        else:
+            self.keyaction_dict[pygame_key_const]['_interval_counter'] = 0
+            return self.keyaction_dict[pygame_key_const]["keyup"]()
 
     def register_keyaction(
             self,
-            pygame_key_const, keydown: Callable, keyup: Callable,
-            delay, interval):
-        keyaction_item = {
+            pygame_key_const,
+            delay, interval,
+            keydown: Callable = lambda: None, keyup: Callable = lambda: None):
+
+        self.keyaction_dict[pygame_key_const] = KeyActionItem({
             "keydown": keydown, "keyup": keyup,
-            "delay": 0, "interval": 0,
-            "is_key_pressed": False}
-        self.keyaction_dict[pygame_key_const] = keyaction_item
-
-    def on_press(self, pygame_event_key, *args, **kwargs):
-        key_action = self.keyaction_dict.get(pygame_event_key)
-        if key_action is not None:
-            return self.keyaction_dict[
-                pygame_event_key]["keydown"](*args, **kwargs)
-
-    def on_release(self, pygame_event_key, *args, **kwargs) -> Callable:
-        return self.keyaction_dict[
-            pygame_event_key]["keyup"]
+            "delay": delay, "interval": interval,
+            "is_pressed": False,
+            "_delay_counter": 0, "_interval_counter": 0})
 
 
-class KeyActionDict(UserDict):
-    pass
+class KeyActionItem(TypedDict):
+    keydown: Callable
+    keyup: Callable
+    delay: int
+    interval: int
+    is_pressed: bool
