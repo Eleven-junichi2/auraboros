db7d461859d6909da458b63cfd38cd0be939cc3f
Eleven-junichi2 <junichinomori@yahoo.co.jp>
2023-03-05 13:09:14 +0900

    rename files

--- 8trail/main.py
+++
@@ -1,8 +0,0 @@
-from eightrail import eightrail
-from eightrail.utilities import AssetFilePath
-
-eightrail.init(pixel_scale=2, caption="8trail",
-               icon_filepath=AssetFilePath.img("icon.ico"))
-
-if __name__ == "__main__":
-    eightrail.run(60)
