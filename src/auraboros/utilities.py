ae3fdc8611569cce4c3087a56573c5233a3110dc
Eleven-junichi2 <junichinomori@yahoo.co.jp>
2023-03-05 18:27:10 +0900

    update utilities.py to deactivate TextToDebug

--- src/auraboros/utilities.py
+++ src/auraboros/utilities.py
@@ -99,37 +99,37 @@ class AssetFilePath:
         return cls.pyinstaller_path(Path(cls.level_dirname) / filename)
 
 
-class TextToDebug:
-    """
-    Example:
-        clock = pygame.time.Clock()
-        surface_object = pygame.surface.Surface((10, 10))
-        TextToDebug.fps(clock) # prepare text before do render()
-        TextToDebug.render("fps", surface_object, (10, 20))
-    """
-    _debug_text_factory = TextSurfaceFactory()
-    _debug_text_factory.register_font(
-        "misaki",
-        pygame.font.Font(AssetFilePath.font("misaki_gothic.ttf"), 16))
-    render = _debug_text_factory.render
+# class TextToDebug:
+#     """
+#     Example:
+#         clock = pygame.time.Clock()
+#         surface_object = pygame.surface.Surface((10, 10))
+#         TextToDebug.fps(clock) # prepare text before do render()
+#         TextToDebug.render("fps", surface_object, (10, 20))
+#     """
+#     _debug_text_factory = TextSurfaceFactory()
+#     _debug_text_factory.register_font(
+#         "misaki",
+#         pygame.font.Font(AssetFilePath.font("misaki_gothic.ttf"), 16))
+#     render = _debug_text_factory.render
 
-    @classmethod
-    def arrow_keys(cls, key):
-        key_text = f"↑{key[pygame.K_UP]}"
-        key_text += f"↓{key[pygame.K_DOWN]}"
-        key_text += f"←{key[pygame.K_LEFT]}"
-        key_text += f"→{key[pygame.K_RIGHT]}"
-        cls._debug_text_factory.register_text("arrow_keys", key_text)
+#     @classmethod
+#     def arrow_keys(cls, key):
+#         key_text = f"↑{key[pygame.K_UP]}"
+#         key_text += f"↓{key[pygame.K_DOWN]}"
+#         key_text += f"←{key[pygame.K_LEFT]}"
+#         key_text += f"→{key[pygame.K_RIGHT]}"
+#         cls._debug_text_factory.register_text("arrow_keys", key_text)
 
-    @classmethod
-    def arrow_keys_from_event(cls, event_key):
-        key_text = f"↑{event_key == pygame.K_UP}"
-        key_text += f"↓{event_key == pygame.K_DOWN}"
-        key_text += f"←{event_key == pygame.K_LEFT}"
-        key_text += f"→{event_key == pygame.K_RIGHT}"
-        cls._debug_text_factory.register_text(
-            "arrow_keys_from_event", key_text)
+#     @classmethod
+#     def arrow_keys_from_event(cls, event_key):
+#         key_text = f"↑{event_key == pygame.K_UP}"
+#         key_text += f"↓{event_key == pygame.K_DOWN}"
+#         key_text += f"←{event_key == pygame.K_LEFT}"
+#         key_text += f"→{event_key == pygame.K_RIGHT}"
+#         cls._debug_text_factory.register_text(
+#             "arrow_keys_from_event", key_text)
 
-    @classmethod
-    def fps(cls, clock: pygame.time.Clock):
-        cls._debug_text_factory.register_text("fps", f"FPS:{clock.get_fps()}")
+#     @classmethod
+#     def fps(cls, clock: pygame.time.Clock):
+#         cls._debug_text_factory.register_text("fps", f"FPS:{clock.get_fps()}")
