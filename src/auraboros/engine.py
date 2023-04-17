5e8a10c01d695c712f1402abf5c1303c6916efc4
Eleven-junichi2 <junichinomori@yahoo.co.jp>
2023-04-17 20:52:08 +0900

    fix lack of argument

--- src/auraboros/engine.py
+++ src/auraboros/engine.py
@@ -31,7 +31,7 @@ def run(scene_manager: SceneManager, fps=60):
     while running:
         dt = clock.tick(fps)
         Schedule.execute()
-        Stopwatch.update_all_stopwatch()
+        Stopwatch.update_all_stopwatch(dt)
         Schedule._debug()
         global_.screen.fill((0, 0, 0))
         if opengl_is_used:
