a14b6b67deb63f1c5b97b7585bd03841e7ff3af2
Eleven-junichi2 <junichinomori@yahoo.co.jp>
2023-04-18 18:34:25 +0900

    make getasset option process

--- src/auraboros/__main__.py
+++ src/auraboros/__main__.py
@@ -1,12 +1,17 @@
+from collections import OrderedDict
 from pathlib import Path
+from urllib.parse import urlparse
+import ssl
 import subprocess
-# import sys
+import urllib.request
+import urllib.error
+import urllib.parse
 import inspect
 
 import click
 
 
-def example():
+def example_process():
     __main__py_path = Path(inspect.getfile(inspect.currentframe()))
     example_dir = __main__py_path.parent / "debugs"
     print(__main__py_path)
@@ -20,8 +25,78 @@ def example():
     subprocess.run(["python", example_scripts[example_num]])
 
 
-def getasset():
-    pass
+def _progress_of_download(block_count: int, block_size: int, total_size: int):
+    progress_bar = click.progressbar(length=total_size, label="Downloading...")
+    downloaded = block_count * block_size
+    if downloaded < total_size:
+        progress_bar.update(downloaded - progress_bar.pos)
+    else:
+        progress_bar.update(total_size - progress_bar.pos)
+    progress_bar.render_progress()
+
+
+def getasset_process():
+    dirname_of_the_asset_type = {
+        "font": "fonts"
+    }
+    asset_dir = click.prompt(
+        "directory to put assets",
+        type=click.Path(file_okay=False, exists=True),
+        default=Path.cwd() / "assets")
+    assets = OrderedDict()
+    assets["misaki"] = \
+        {"url": "https://littlelimit.net/arc/misaki/misaki_ttf_2021-05-05.zip",
+         "type": "font",
+         "license": "Read the attached file."}
+    assets["ayu18mincho"] = \
+        {"url": "https://ja.osdn.net/frs/redir.php?m=nchc&f=x-tt%2F8494%2Fayu18mincho-1.1.tar.gz",
+         "type": "font",
+         "license": "public"}
+    for i, (asset_name, asset_info) in enumerate(
+            zip(assets.keys(), assets.values())):
+        click.echo(f"{i} {asset_name} ({asset_info['type']})")
+    request_num = click.prompt(
+        "Choose the asset you wish to use", type=int, default=0)
+    asset_name = list(assets.keys())[request_num]
+    asset_info = list(assets.values())[request_num]
+    click.echo(
+        f"{asset_name}({asset_info['type']})" +
+        f"\nlicense: {asset_info['license']}")
+    is_confirm = click.confirm("Are you sure to download?")
+    if not is_confirm:
+        return
+    url = asset_info["url"]
+    context = ssl.create_default_context()
+    domain = urlparse(url).netloc
+    if domain == "ja.osdn.net":
+        context.check_hostname = False
+        context.verify_mode = ssl.CERT_NONE
+    try:
+        with urllib.request.urlopen(url, context=context) as response:
+            content_disposition = response.headers.get("Content-Disposition")
+            if content_disposition:
+                filename = content_disposition.split('filename=')[1].strip('"')
+            else:
+                filename = Path(urllib.parse.unquote(url.split('/')[-1])).name
+            print(filename)
+            download_to = asset_dir / \
+                dirname_of_the_asset_type[asset_info["type"]] / filename
+            with open(download_to, mode="wb") as f:
+                content = response.read()
+                f.write(content)
+    except urllib.error.HTTPError as e:
+        click.echo(f'HTTPError: {e.code} {e.reason}')
+    except urllib.error.URLError as e:
+        click.echo(f'URLError: {e.reason}')
+    else:
+        click.echo(
+            click.style(
+                f"Download completed successfully\n-> {download_to}",
+                fg="green"))
+        click.echo(
+            click.style(
+                "Be careful: You must comply with licence of the assets.",
+                fg="red"))
 
 
 @click.command()
@@ -31,9 +106,9 @@ def getasset():
               help="download assets into assets at current working directory")
 def cli(example, getasset):
     if example:
-        example()
+        example_process()
     elif getasset:
-        getasset()
+        getasset_process()
     else:
         click.echo(click.get_current_context().get_help())
 
