7527020ada2f236c39ee56a2e1a36ba3331e2a78
Eleven-junichi2 <junichinomori@yahoo.co.jp>
2023-05-04 19:28:17 +0900

    failed tests/test_gametext.py

--- tests/test_gametext.py
+++ tests/test_gametext.py
@@ -1,5 +1,7 @@
-from src.auraboros.gametext import split_multiline_text, \
-    line_count_of_multiline_text
+from src.auraboros.gametext import (
+    split_multiline_text,
+    line_count_of_multiline_text,
+)
 
 
 def test_split_multiline_text():
@@ -14,8 +16,12 @@ def test_split_multiline_text():
 
 def test_line_count_of_multiline_text():
     height = line_count_of_multiline_text(
-        "AaBbCcDdEe\nFfGg\nHhIiJjKkLlMmNnOoPp", 12)
+        "AaBbCcDdEe\nFfGg\nHhIiJjKkLlMmNnOoPp", 12
+    )
     assert height == 4
-    height = line_count_of_multiline_text(
-        "", 12)
+    height = line_count_of_multiline_text("", 12)
     assert height == 1
+    height = line_count_of_multiline_text(
+        "AaBbCcDdEe\n\n\nFfGg\nHhIiJjKkLlMmNnOoPp", 12
+    )
+    assert height == 6
