2fcb111966c098a38497ee558fa1db392e5d3b41
Eleven-junichi2 <junichinomori@yahoo.co.jp>
2023-05-06 21:30:55 +0900

    update doc

--- src/auraboros/debugs/msgbox.py
+++ src/auraboros/debugs/msgbox.py
@@ -1,7 +1,7 @@
-
 # from collections import deque
 from pathlib import Path
 import sys
+
 # from string import ascii_lowercase
 
 import pygame
@@ -17,10 +17,10 @@ engine.init(caption="Test MsgWindow System", pixel_scale=2)
 
 AssetFilePath.set_asset_root(Path(sys.argv[0]).parent / "assets")
 
+GameText.setup_font(Font2(AssetFilePath.font("misaki_gothic.ttf"), 16), "misakigothic")
 GameText.setup_font(
-    Font2(AssetFilePath.font("misaki_gothic.ttf"), 16), "misakigothic")
-GameText.setup_font(
-    Font2(AssetFilePath.font("ayu18gothic/9x18gm.bdf"), 16), "ayu18gothic")
+    Font2(AssetFilePath.font("ayu18gothic/9x18gm.bdf"), 16), "ayu18gothic"
+)
 
 
 class DebugScene(Scene):
@@ -30,41 +30,50 @@ class DebugScene(Scene):
         self.msgbox = MsgWindow(
             GameText.font,
             "frame_width=4\nmultiline text MULTILINE TEXT",
-            singleline_length=13, sizing_style="fixed_if_larger_than_min",
-            frame_width=4)
+            singleline_length=13,
+            sizing_style="fixed_if_larger_than_min",
+            frame_width=4,
+        )
         self.msgbox.padding = 4
-        self.msgbox.fixed_size[0] = global_.w_size[0]*0.95
-        self.msgbox.fixed_size[1] = global_.w_size[1]*0.22
-        self.msgbox.pos[1] = global_.w_size[1]*0.975 - self.msgbox.real_size[1]
+        self.msgbox.fixed_size[0] = global_.w_size[0] * 0.95
+        self.msgbox.fixed_size[1] = global_.w_size[1] * 0.22
+        self.msgbox.pos[1] = global_.w_size[1] * 0.975 - self.msgbox.real_size[1]
         self.msgbox.set_x_to_center()
 
         self.msgbox2 = MsgWindow(
             GameText.font,
-            ["click or down up to next | (text 1)",
-             "(text 2)",
-             "wheel up to back | (text 3)"],
-            sizing_style="fixed_if_larger_than_min")
+            [
+                "click or down up to next | (text 1)",
+                "(text 2)",
+                "wheel up to back | (text 3)",
+            ],
+            sizing_style="fixed_if_larger_than_min",
+        )
         self.msgbox2.padding = 4
-        self.msgbox2.fixed_size[0] = global_.w_size[0]*0.95
-        self.msgbox2.fixed_size[1] = global_.w_size[1]*0.22
-        self.msgbox2.pos[1] = self.msgbox.pos[1] * \
-            0.95 - self.msgbox2.real_size[1]
+        self.msgbox2.fixed_size[0] = global_.w_size[0] * 0.95
+        self.msgbox2.fixed_size[1] = global_.w_size[1] * 0.22
+        self.msgbox2.pos[1] = self.msgbox.pos[1] * 0.95 - self.msgbox2.real_size[1]
         self.msgbox2.set_x_to_center()
 
         self.mouse.register_mouseaction(
             "down",
             on_left=lambda: self.msgbox2.do_func_if_pos_is_on_ui(
                 pos_on_pixel_scale(pygame.mouse.get_pos()),
-                lambda: self.msgbox2.change_text_to_next()))
+                lambda: self.msgbox2.change_text_to_next(),
+            ),
+        )
 
         self.mouse.register_mouseaction(
             "down",
             on_wheel_down=lambda: self.msgbox2.do_func_if_pos_is_on_ui(
                 pos_on_pixel_scale(pygame.mouse.get_pos()),
-                lambda: self.msgbox2.change_text_to_next()),
+                lambda: self.msgbox2.change_text_to_next(),
+            ),
             on_wheel_up=lambda: self.msgbox2.do_func_if_pos_is_on_ui(
                 pos_on_pixel_scale(pygame.mouse.get_pos()),
-                lambda: self.msgbox2.rewind_text()))
+                lambda: self.msgbox2.rewind_text(),
+            ),
+        )
 
     def draw(self, screen):
         draw_grid(screen, 16, (78, 78, 78))
