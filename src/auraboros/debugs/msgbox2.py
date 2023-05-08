cfdc413b970be8ca71e644a9c3631a3b45fde595
Eleven-junichi2 <junichinomori@yahoo.co.jp>
2023-05-08 22:45:23 +0900

    init new ui module

---
+++ src/auraboros/debugs/msgbox2.py
@@ -0,0 +1,44 @@
+# from collections import deque
+from pathlib import Path
+import sys
+
+# from string import ascii_lowercase
+
+import pygame
+
+import setup_syspath  # noqa
+from auraboros import engine, global_
+from auraboros.gametext import GameText, Font2
+from auraboros.gamescene import Scene, SceneManager
+from auraboros.ui import MsgBox
+from auraboros.utilities import AssetFilePath, draw_grid
+
+engine.init(caption="Test MsgBox", pixel_scale=2)
+
+AssetFilePath.set_asset_root(Path(sys.argv[0]).parent / "assets")
+
+GameText.setup_font(
+    Font2(AssetFilePath.font("PixelMPlus/PixelMplus12-Regular.ttf"), 24),
+    "PixelMplus12Regular",
+)
+
+
+class DebugScene(Scene):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        GameText.use_font("PixelMplus12Regular")
+        self.msgbox1 = MsgBox(
+            GameText.font,
+            "test",
+        )
+
+    def draw(self, screen):
+        draw_grid(screen, 16, (78, 78, 78))
+        self.msgbox1.draw(screen)
+
+
+scene_manager = SceneManager()
+scene_manager.push(DebugScene(scene_manager))
+
+if __name__ == "__main__":
+    engine.run(scene_manager=scene_manager, fps=60)
