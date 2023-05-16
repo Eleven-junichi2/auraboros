8cce61ed488091fb5c3678e3e54435ea6bd7ed9b
Eleven-junichi2 <junichinomori@yahoo.co.jp>
2023-05-16 20:17:17 +0900

    make test for ecs and fix ecs

--- src/auraboros/ecs/common.py
+++ src/auraboros/ecs/common.py
@@ -1,6 +1,8 @@
 from .component import Component
 from .system import System
 
+import pygame
+
 Velocity = Component("velocity", [0, 0])
 Position = Component("position", [0, 0])
 Size = Component("size", [1, 1])
@@ -8,15 +10,19 @@ HealthPoint = Component("health_point", 1)
 ExperientPoint = Component("experient_point", 1)
 Hp = HealthPoint
 Exp = ExperientPoint
+Surface = Component("pygame_surface", )
 
 
 class MovementSystem(System):
     def do(self, *args, **kwargs):
         for entity in self.world.get_entities():
-            self.world.edit_entity[entity]["position"].value = map(
+            self.world.edit_entity[entity][Position.name].value = map(
                 sum,
                 zip(
                     self.world.component_for_entity(entity, "position"),
                     self.world.component_for_entity(entity, "velocity"),
                 ),
             )
+
+a = {"abc": {"ABC": "aiueo"}}
+a["abc"]["ABC"]
